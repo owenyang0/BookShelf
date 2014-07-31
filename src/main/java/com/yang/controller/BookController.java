2724554df195639b2a0c64b51306ecf5f68bd1d2
OwenYang <usay@outlook.com>
2014-07-31 21:22:22 +0800

    structure: project structure redesign

--- src/main/java/com/yang/controller/BookController.java
+++ src/main/java/com/yang/controller/BookController.java
@@ -10,12 +10,12 @@ import yang.DBCPManager;
  * Created by songyang on 7/24/14.
  */
 @Controller
-@RequestMapping("show/books")
+@RequestMapping("books/show")
 public class BookController {
     @RequestMapping(method = RequestMethod.GET)
     public String getBooks(Model model) {
         model.addAttribute("books", DBCPManager.select("*"));
         System.out.println(model);
-        return "books";
+        return "show";
     }
 }
--- src/main/java/yang/ShowBook.java
+++ src/main/java/yang/ShowBook.java
@@ -12,6 +12,6 @@ import java.io.IOException;
 public class ShowBook extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("WEB-INF/views/books.jsp").forward(req, resp);
+        req.getRequestDispatcher("WEB-INF/views/books/show.jsp").forward(req, resp);
     }
 }
